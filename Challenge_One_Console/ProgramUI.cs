ce47cea550de0728116f8d3a0de7f202c8aca8e9
Richard Torres <torresrichard252@gmail.com>
2020-12-05 14:32:13 -0500

    Add project files.

---
+++ Challenge_One_Console/ProgramUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_One_Console
+{
+    class ProgramUI
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ Challenge_One_Tests/CafeRepoTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Challenge_One_Tests
+{
+    [TestClass]
+    public class CafeRepoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
